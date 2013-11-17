edc559fcf5127e6e08ee8df258cf4f05e55d99f4
Maria Claudia Bodino <mariaclaudia.bodino@gmail.com>
2013-11-17 17:22:06 +0100

    Git exercise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Exercise::Application.config.secret_key_base = 'a8ce6c87071ec2e27196ab5d6eaf65bf884a0aaa412f6e686e20d395f8fc4168c2c56b4d0736d79fc74bbf677cc3c9ca64d74100676c0b3732c8a2504fb106ae'
